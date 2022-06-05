320826a6f308bd28bd59ec9392bf7ce8cc14d634
Olga Nadongo <winnienadongo@gmail.com>
2022-06-05 19:57:01 +0300

    complete assignment e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let newArray = [];
+function writeCards (cards) {
+    for (let i = 0; i < cards.length; i++) {
+      
+        newArray.push (`Thank you, ${cards[i]}, for the wonderful surprise gift!`); 
+       
+    }
+    
+}
+
+function CountDown(integer){
+  while (integer >= 0) {
+    console.log(integer);
+   integer--;
+  }
+}
+CountDown(3);
